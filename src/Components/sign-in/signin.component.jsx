e0550a0f973d9c666304289953d90fc174772183
zee1997 <awan.zeerak27@gmail.com>
2021-01-22 18:58:47 +0500

    Updating signin page styles

--- src/Components/sign-in/signin.component.jsx
+++ src/Components/sign-in/signin.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import FormInput from '../form-input/form.input.component';
+import CustomButton from '../custom-button/custom.button.component';
+
 import './signin.styles.scss';
 
 class SignIn extends React.Component {
@@ -26,15 +29,13 @@ class SignIn extends React.Component {
         return(
             <div className='sign-in'>
                 <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
+                <h4>Sign in with your email and password</h4>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input type='email' name='email' value={this.state.email} onChange={this.handleChange} required/>
-                    <label>Email</label>
-                    <input type='password' name='password' value={this.state.password} onChange={this.handleChange} required />
-                    <label>Password</label>
+                    <FormInput type='email' name='email' value={this.state.email} label='Email' handleChange={this.handleChange} required/>
+                    <FormInput type='password' name='password' value={this.state.password} label='Password' handleChange={this.handleChange} required />
 
-                    <input type='submit' value='Submit Form'/>
+                    <CustomButton type='submit'>Sign In</CustomButton>
                 </form>
             </div>
         );
