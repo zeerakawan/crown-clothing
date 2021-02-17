83289d570306ad547a1ea9ecb6e4e618e4032e79
zee1997 <awan.zeerak27@gmail.com>
2021-02-17 18:55:04 +0500

    Seperate collection overview component with shop page

--- src/Pages/shop/shop.component.jsx
+++ src/Pages/shop/shop.component.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../Components/collection-preview/collection.preview.component';
+import CollectionOverview from '../../Components/collection-overview/collection.overview.component';
 
-import { selectCollections } from '../../redux/shop/shop.selectors';
-
-const ShopPage = ({ collections }) => (
+const ShopPage = () => (
     <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
+        <CollectionOverview/>
     </div>
 );
 
-const mapStateToProps = createStructuredSelector ({
-    collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage); 
+export default ShopPage; 
